08909f6e4abc2c6503a19117ce4ecd51230da1e6
VukasinStankovic <vule.stankovic2@gmail.com>
2022-07-06 09:46:31 +0200

    First update

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
-include 'templates/head.php';
-include 'templates/header.inc.php';
-include 'templates/home.php';
+session_start();
+// print($_SERVER['PHP_SELF']); exit;
+define("DIR_CORE", "core/");
+
+include (DIR_CORE . "constants.php");
+include (DIR_CORE . "functions.php");
 
 
+include (DIR_MODULES . "router.php");
+include (DIR_MODULES . "db.php");
+
+include (DIR_TEMPLATES . "head.php");
+include (DIR_TEMPLATES . "header.php");
+include (DIR_TEMPLATES . "page_{$page}.php");
+include (DIR_TEMPLATES . "footer.php");
 
 
-include 'templates/footer.inc.php';
-?>
+mysqli_close($connect);
